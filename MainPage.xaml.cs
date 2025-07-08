99880fc571a5ce324e9968bad6169eabb1a594cf
halosakop <150012023+halosakop@users.noreply.github.com>
2025-07-08 20:36:28 +0200

    test

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -13,7 +13,7 @@ public partial class MainPage : ContentPage
 	{
 		count++;
 
-		if (count == 2)
+		if (count == 1)
 			CounterBtn.Text = $"Clicked {count} time";
 		else
 			CounterBtn.Text = $"Clicked {count} times";
