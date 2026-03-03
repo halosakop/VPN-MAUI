db28bd947f1824ead8460cefabc047282ee2e88e
halosakop <150012023+halosakop@users.noreply.github.com>
2026-03-03 10:43:11 +0100

    update na .net10

--- View/LoginPage.xaml.cs
+++ View/LoginPage.xaml.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+using CommunityToolkit.Maui.Extensions;
+using CommunityToolkit.Maui.Views;
 using VPN_MAUI.Model;
-using WebKit;
 
 namespace VPN_MAUI.View;
 
@@ -15,25 +10,25 @@ public partial class LoginPage : ContentPage
     {
         InitializeComponent();
     }
-
+    
     private async void BtnLogin_OnClicked(object? sender, EventArgs e)
     {
         string meno = MenoLogin.Text;
         string heslo = HesloLogin.Text;
+
         
         if (await PostgresLogin.Login(meno, heslo))
         {
-           await Shell.Current.GoToAsync(nameof(HomePage));
+            await Shell.Current.GoToAsync(nameof(HomePage));
         }
         else
         {
-            DisplayAlert("", "Nesprávne prihlasovacie údaje", "OK");
+            await DisplayAlertAsync("", "Nesprávne prihlasovacie údaje", "OK");
         }
     }
 
     private void BtnNoLogin_OnClicked(object? sender, EventArgs e)
     {
         Shell.Current.GoToAsync("//" + nameof(StartPage));
-        
     }
 }
