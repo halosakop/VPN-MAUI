2d95e393acf3e603a1ad1cbedc7c0df1a223c812
halosakop <150012023+halosakop@users.noreply.github.com>
2026-04-22 13:50:08 +0200

    GUI for StartPagev1

--- View/StartPage.xaml.cs
+++ View/StartPage.xaml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,5 +22,15 @@ public partial class StartPage : ContentPage
     private void PrihlasenieDoUctuButton_OnClicked(object? sender, EventArgs e)
     {
         Shell.Current.GoToAsync(nameof(LoginPage));
+    }
+
+    private void PointerGestureRecognizer_OnPointerEntered(object? sender, PointerEventArgs e)
+    {
+        PrihlasenieDoUctuButton.TextColor = Colors.Black;
+    }
+
+    private void PointerGestureRecognizer_OnPointerExited(object? sender, PointerEventArgs e)
+    {
+        PrihlasenieDoUctuButton.TextColor = Colors.White;
     }
 }
